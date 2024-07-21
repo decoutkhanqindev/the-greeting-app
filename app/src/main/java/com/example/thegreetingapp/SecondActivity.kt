f73c23dd16d9a9939bd3477304de1d6e29cc7dc3
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-21 23:38:38 +0700

    declare and initializing views in second activity

--- app/src/main/java/com/example/thegreetingapp/SecondActivity.kt
+++ app/src/main/java/com/example/thegreetingapp/SecondActivity.kt
@@ -1,12 +1,20 @@
 package com.example.thegreetingapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SecondActivity : AppCompatActivity() {
+    lateinit var nameReceiver: TextView
+    lateinit var languageReceiver: TextView
+    lateinit var imgReceiver: ImageView
+    lateinit var moveToGoogle: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,10 @@ class SecondActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        nameReceiver = findViewById(R.id.nameReceiver)
+        languageReceiver = findViewById(R.id.languageReceiver)
+        imgReceiver = findViewById(R.id.imgReceiver)
+        moveToGoogle = findViewById(R.id.moveToGoogle)
     }
 }
