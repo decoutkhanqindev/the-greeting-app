e5809dcd78e8c625de43fc1b80f421677f92b5f3
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-21 15:25:58 +0700

    declare and initializing views

--- app/src/main/java/com/example/thegreetingapp/MainActivity.kt
+++ app/src/main/java/com/example/thegreetingapp/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.thegreetingapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var inputName: EditText
+    private lateinit var inputLanguage: EditText
+    private lateinit var btn: Button
+    private lateinit var imgLanguage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        inputName = findViewById(R.id.inputName)
+        inputLanguage = findViewById(R.id.inputLanguage)
+        btn = findViewById(R.id.btn)
+        imgLanguage = findViewById(R.id.languageImg)
+
+        btn.setOnClickListener {
+            Toast.makeText(this, "Hello ${inputName.text}", Toast.LENGTH_LONG ).show()
+
+            if (inputLanguage.text.toString().lowercase() == "java") {
+                imgLanguage.setImageResource(R.drawable.java)
+            } else {
+                imgLanguage.setImageResource(R.drawable.kotln)
+            }
         }
     }
 }
